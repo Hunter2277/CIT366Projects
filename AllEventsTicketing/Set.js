5eb27d5b9f457493828da6de6c9907ca5b37d1b3
Erika Hunter <erika22hunter@outlook.com>
2017-04-26 17:57:40 -0600

    4/26/2017 set.js is now functioning

--- AllEventsTicketing/Set.js
+++ AllEventsTicketing/Set.js
@@ -1,56 +1,110 @@
 function Set() {
-	
-	
-	this.intersection = function(listA, listB) {
-    
-	   var resultList = new Array();
-       
-	   /*-------------------------------Insert your code here -------------------------------------*/
-	   
-	   /*-------------------------------Insert your code here -------------------------------------*/
-       
-	   return resultList;
-	}
-    
-    
-    
-	this.union = function(listA, listB) {
 
-	   var resultList = new Array();
-       
-	   /*-------------------------------Insert your code here -------------------------------------*/
-	   
-	   /*-------------------------------Insert your code here -------------------------------------*/ 
-	   
-	   return resultList;
-	}
+
+    this.intersection = function (listA, listB) {
+
+        var resultList = [];
+
+        if (listA === null || listB === null) {
+            return null;
+        }
+
+        for (var i = 0; i < listA.length; i++) {
+            var nextValue = listA[i];
+
+            for (var j = 0; j < listB.length; j++) {
+                if (listB[j] === nextValue) {
+                    resultList.push(listB[j]);
+                    break;
+                }
+            }
+        }
+        return resultList;
+    }
 
 
 
 
-	this.relativeComplement = function(listA, listB) {
+     this.union = function(listA, listB) {
 
-	   var resultList = new Array();
-       
-	   /*-------------------------------Insert your code here -------------------------------------*/
-	   	   
-	   /*-------------------------------Insert your code here -------------------------------------*/
-       
-	   return resultList;
-	}
+     var resultList = [];
+     if(listA === null || listB === null){
+        return null;
+     }
+     var r1 = this.symmetricDifference(listA,listB);
+         for(var i =0; i < r1.length; i++){
+         resultList.push(r1[i]);
+         }
+         var r2 = this.intersection(listA,listB);
+        for(var j = 0; j < r2.length; j++){
+             resultList.push(r2[j]);
+        }
+
+     return resultList;
+     }
+
+
+    this.relativeComplement = function (listA, listB) {
+
+        var resultList = [];
+
+        if (listA === null || listB === null) {
+            return null;
+        }
+
+        for (var i = 0; i < listA.length; i++) {
+            var nextValue = listA[i];
+
+            for (var j = 0; j < listB.length; j++) {
+                if (listB[j] === nextValue) {
+                    break;
+                }
+            }
+            if (listB[j] !== nextValue) {
+                resultList.push(listA[i]);
+            }
+        }
+
+        return resultList;
+    }
 
 
 
-	this.symmetricDifference = function(listA, listB) {
+    this.symmetricDifference = function (listA, listB) {
+        var resultList = [];
 
-	   var resultList = new Array();
-       
-	   /*-------------------------------Insert your code here -------------------------------------*/
+        if (listA === null || listB === null) {
+            return null;
+        }
 
-	   /*-------------------------------Insert your code here -------------------------------------*/
-       
-	   return resultList;
-	}	
-	
+
+        var r1 = this.relativeComplement(listA, listB);
+
+        for (var i = 0; i < r1.length; i++) {
+            resultList.push(r1[i]);
+        }
+
+
+        var r2 = this.relativeComplement(listB, listA);
+
+        for (var j = 0; j < r2.length; j++) {
+            resultList.push(r2[j]);
+        }
+
+        return resultList;
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+	
+
+
