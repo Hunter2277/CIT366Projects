8c2e43d5ee8f14ac4e30e23db07e7fe321819f4f
Erika Hunter <erika22hunter@outlook.com>
2017-06-02 13:15:31 -0600

    6/2/2017 Lesson 4 completed

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -6,7 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
+  documents: Document[]= [
+    new Document(
+      '1',
+      'Analysis of Fidgit Spinners',
+    'Research Paper',
+  'fidgit.pdf',
+    null),
+    new Document(
+      '2',
+      'North Korean Nuclear Threat',
+      'Report',
+      'northK.pdf',
+      null),
+    new Document(
+      '3',
+      'Why Humanity is Drowning',
+      'Essay',
+      'humanity.pdf',
+      null),
+    new Document(
+      '4',
+      'The life and Journey of John Hancock',
+      'Bio',
+      'Hancock.pdf',
+      null)
+    ];
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
   constructor() { }
 
   ngOnInit() {
